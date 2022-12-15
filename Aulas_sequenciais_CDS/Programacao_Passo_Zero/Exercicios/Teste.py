a7360cd2888e23acfbc5bad5a40d9f5254fb94bb
Conrado <conradobolonha@gmail.com>
2022-12-15 10:39:15 -0300

    v.1.00

---
+++ Aulas_sequenciais_CDS/Programacao_Passo_Zero/Exercicios/Teste.py
@@ -0,0 +1,4 @@
+qtade_real = float(input('Digite a quantidade de reais: R$ '))
+vl_dolar = 5.30
+conv_dol_real = qtade_real * vl_dolar
+print(f'R$ {qtade_real:.2f} corresponde a U$ {conv_dol_real:.2f}')
