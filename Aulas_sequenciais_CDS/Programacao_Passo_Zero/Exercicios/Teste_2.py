ad72da181f64268b6796a3f2f5861371d68854b2
Conrado <conradobolonha@gmail.com>
2022-12-19 17:16:07 -0300

    v.1.00

--- Aulas_sequenciais_CDS/Programacao_Passo_Zero/Exercicios/Teste_2.py
+++ Aulas_sequenciais_CDS/Programacao_Passo_Zero/Exercicios/Teste_2.py
@@ -1,7 +1,15 @@
-
 print()
-num = int(input('Digite um número: '))
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite um segundo valor: '))
+n3 = int(input('Digite um trerceiro valor: '))
+n4 = int(input('Digite um quarto valor: '))
 
-for cont in range (1, 11):
-    print(f'{num} X {cont} = {num * cont}')
+if n1 > n2 > n3 > n4:
+    print(f'{n1}')
+    if n2 > n1 > n3 > n4:
+        print(f'{n2}')
+    if n3 > n1 > n2 > n4:
+        print(f'{n3}')
+    else:
+        print(n4)
 print()
