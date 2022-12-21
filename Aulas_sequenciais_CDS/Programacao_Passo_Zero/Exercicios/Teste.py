629221aaf44fc4874f5e008892302fe44247c7ef
Conrado <conradobolonha@gmail.com>
2022-12-21 17:51:20 -0300

    v.1.00

--- Aulas_sequenciais_CDS/Programacao_Passo_Zero/Exercicios/Teste.py
+++ Aulas_sequenciais_CDS/Programacao_Passo_Zero/Exercicios/Teste.py
@@ -1,9 +1,4 @@
-print()
-while True:
-    num = int(input('Digite um número: '))
-    if num < 0:
-        break
-    for cont in range(1, 11):
-        print(f'{num} X {cont + 1} = {num * cont}')
-print('Número negativo não permitido!')
-print()
+lista_a = ['Conrado', 45, 1.83, 95]
+
+for impress in lista_a:
+    print(impress)
