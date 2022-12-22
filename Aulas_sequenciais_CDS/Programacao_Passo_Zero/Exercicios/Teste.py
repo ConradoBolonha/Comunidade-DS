de3b8b2bcc8fa80ff32e0ee2011d232466299811
Conrado <conradobolonha@gmail.com>
2022-12-22 10:36:56 -0300

    v.1.00

--- Aulas_sequenciais_CDS/Programacao_Passo_Zero/Exercicios/Teste.py
+++ Aulas_sequenciais_CDS/Programacao_Passo_Zero/Exercicios/Teste.py
@@ -1,4 +1,7 @@
-lista_a = ['Conrado', 45, 1.83, 95]
+list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+list_ap = []
 
-for impress in lista_a:
-    print(impress)
+for cont in list_1:
+    if cont % 2 != 0:
+        list_ap.append(cont)
+print(f'Os números são {list_ap}')
