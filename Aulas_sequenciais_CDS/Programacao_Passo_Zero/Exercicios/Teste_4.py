0920cc3eef09fa5799953ad720dfd9510bbeec96
Conrado <conradobolonha@gmail.com>
2022-12-23 15:58:03 -0300

    v.1.00

---
+++ Aulas_sequenciais_CDS/Programacao_Passo_Zero/Exercicios/Teste_4.py
@@ -0,0 +1,6 @@
+lista = ['oi', 'olá', 'vaza', 'fui', 'dia', 'notebook', 'teste']
+
+for cont, teste in enumerate(lista):
+    print(cont, teste)
+
+# O atributo "enumerate", mostra os índices em que os objetos estão.
---
+++ Aulas_sequenciais_CDS/Programacao_Passo_Zero/Exercicios/Teste_5.py
@@ -0,0 +1,4 @@
+list_obj = ['caneta', 'pincel', 'cinzeiro', 'hd', 'copo', 'agenda', 'papel']
+
+for contador, copo in enumerate(list_obj):
+    print(contador, copo)
