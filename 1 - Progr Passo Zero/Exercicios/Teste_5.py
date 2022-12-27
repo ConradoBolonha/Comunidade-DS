6fd559e6bfb04833b47432437124d98458ac8a1e
Conrado <conradobolonha@gmail.com>
2022-12-27 16:34:34 -0300

    v.1.00

---
+++ 1 - Progr Passo Zero/Exercicios/Teste_5.py
@@ -0,0 +1,11 @@
+
+
+num = int(input('Digite um número: '))
+
+lista_num = [n1, n2, n3, n4]
+media = (sum(lista_num)) / 4
+
+print(f'O número maior é {max(lista_num)}')
+print(f'O número menor é {min(lista_num)}')
+print(f'A soma dos números: {sum(lista_num)}')
+print(f'A média dos números: {media}')
--- Aulas_sequenciais_CDS/Programacao_Passo_Zero/Exercicios/Teste_5.py
+++
@@ -1,4 +0,0 @@
-list_obj = ['caneta', 'pincel', 'cinzeiro', 'hd', 'copo', 'agenda', 'papel']
-
-for contador, copo in enumerate(list_obj):
-    print(contador, copo)
